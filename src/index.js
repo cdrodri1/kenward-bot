c7061a165775b1ee45454e0976ab0e4c8d2920c6
hkennyv <hkennyv@gmail.com>
2017-10-08 01:41:45 -0700

    added personality responses and reddit image function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
-const YTDL = require('ytdl-core');
 const client = new Discord.Client();
+
+const request = require('request');
+const YTDL = require('ytdl-core');
+const personality = require('./personality.js');
+
 
 
 const token = 'MzY1MjE4Mzg5MTcxMzcyMDQ1.DLbHsA.7aTq6ntvepp4r1LQlEsrZXUQIrY';
-
-// regex
-const cmdRegex = /(!hello|!ask|!play|!speak) (.*)$/;
 
 var servers = {};
 const people = ['elias', 'neil', 'chad', 'daniel', 'kenward'];
@@ -17,78 +18,70 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// create event listener for each person
-people.forEach(function(person){
-  client.on('message', message =>{
-  	if(message.content === person){
-  		console.log(person + ' spoke!');
-  		message.channel.send(person + ' is a faggot'); 
-  	}
-  });
-})
-
-client.on('message', m =>{
-	console.log(counter);
-  if(m.content === 'hello'){
-  	counter++;
-  	console.log('hello was said ' +counter+ ' times.');
-  }
-  if(counter===10){
-  	m.channel.send('you said hello ' +counter+ ' times.');
-  	counter = 0; 
-  }
-});
-
-client.on('message', message => {
-	if(message.author.username === 'kenward' && message.content === 'hello kenward bot'){
-		message.channel.send('hello master kenward', {tts:true});
+client.on('message', m => {
+	if(m.author.username === 'kenward'){
+		// m.channel.send('hello master kenward', {tts:true});
+		if(personality.responses[m.content]){
+			m.channel.send(personality.responses[m.content.toLowerCase()]);
+		}
 	}
 });
 
 // cmd listener
-client.on('message', message =>{
-	const input = message.content.split(' '); 
-	console.log(input);
+client.on('message', m =>{
+	const input = m.content.split(' '); 
   if(input){	
   	switch(input[0]){
 
   		case '!hello': 
-  			if(input[1]) message.channel.send('Hey there, ' + input[1]);
-  			else message.reply(' how\'re ya doing?');
+  			if(input[1]) m.channel.send('Hey there, ' + input[1]);
+  			else m.reply(' how\'re ya doing?');
   			break;
 
 			case '!ask':
-				message.channel.send('What?');
+				m.channel.send('What?');
+				break;
+
+			case '!roll':
+				diceRoll(m, input[1]);
+				break;
+
+			case '!chrissy':
+				postChrissy(m);
+				break;
+
+			case '!reddit':
+				postPic(m);
 				break;
 
 			case '!play':
-				message.channel.send('woof woof!');
+				m.channel.send('woof woof!');
 				if(!input[1]){
-					message.channel.send('Please send a link');
+					m.channel.send('Please send a link');
 					return;
 				}
-				if(!message.member.voiceChannel){
-					message.channel.send('Please join a voice channel');
+				if(!m.member.voiceChannel){
+					m.channel.send('Please join a voice channel');
 				}
-				if(!servers[message.guild.id]) servers[message.guild.id] = {
+				if(!servers[m.guild.id]) servers[m.guild.id] = {
 					queue : []
 				};
-				var server = servers[message.guild.id];
+				var server = servers[m.guild.id];
 				server.queue.push(input[1]);
 				console.log('queue: ' + server.queue);
-				if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-					play(connection, message);
+				if(!m.guild.voiceConnection) m.member.voiceChannel.join().then(function(connection){
+					play(connection, m);
 				});
 				break;
 
 			case '!skip':
-				var server = servers[message.guild.id];
+				var server = servers[m.guild.id];
 				if(server.dispatcher) server.dispatcher.end();
 				break;
 
 			case 'stop': 
-				var server = servers[message.guild.id];
-				if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+				var server = servers[m.guild.id];
+				if(m.guild.voiceConnection) m.guild.voiceConnection.disconnect();
 				break; 
 
 			default:
@@ -98,17 +91,65 @@ client.on('message', message =>{
 });
 
 
-function play(connection, message){
-	var server = servers[message.guild.id];
+function play(connection, m){
+	var server = servers[m.guild.id];
 	server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter:'audioonly'}));
 	server.dispatcher.setVolume(0.5);
 	server.queue.shift();
 	server.dispatcher.on('end', function(){
-		if(server.queue[0]) play(connection, message);
+		if(server.queue[0]) play(connection, m);
 		else connection.disconnect();
 	});
 }
 
+function diceRoll(message, num){
+	if(Number(num)){
+		message.reply('Rolling... ' + (Math.floor(Math.random() * (Number(num) - 1)) + 1));
+	} else{
+		message.reply('Rolling... ' + (Math.floor(Math.random() * (6 - 1)) + 1));
+	}
+}
+
+function postChrissy(message){
+	let posts = []; 
+	request('https://www.reddit.com/r/ChrissyTeigen/.json', function(err, resp, body){
+		let post = JSON.parse(body);
+		for(let i = 0; i<20; i++){
+			posts.push(post.data.children[i].data.preview.images[0].source.url);
+		}
+		message.channel.send(posts[Math.floor(Math.random()*(20))]);
+	});
+}
+
+function postPic(message){
+	let m = message.content.split(' ');
+	let sub = m[1]; 
+	let domains = ['i.redd.it', 'i.imgur.com', 'gfycat.com', 'v.redd.it'];
+	let posts = []; 
+	console.log(m);
+	request('https://www.reddit.com/r/'+sub+'/.json?limit=50', function(err, resp, body){
+		if(err){
+			console.log(err);
+			message.channel.send("That's not a subreddit!");
+		} else{
+			let post = JSON.parse(body);
+			for(let i = 0; i<50; i++){
+				console.log(i);
+				if(domains.indexOf(post.data.children[i].data.domain) > -1){
+					posts.push(post.data.children[i]);
+				}
+			}
+			let n = Math.floor(Math.random()*(posts.length)); 
+			console.log(posts.length, n);
+			if(posts.length > 0){
+			message.channel.send('`' + posts[n].data.title + '`');
+			message.channel.send(posts[n].data.url);
+			} else{
+				message.channel.send('No images in /r/'+m[1]+' :(');
+			}
+		}	
+	});
+}
 
 
 // log bot in
---
+++ src/personality.js
@@ -0,0 +1,17 @@
+// personality.js
+
+const personality = {
+	responses: {
+		'i love you' : 'I love you too.',
+		'how are you doing?' : 'I am doing quite well for a bot, thanks for asking!', 
+		'what\'s up?' : 'Your mother.', 
+		'thanks kenward bot' : 'You\'re welcome.',
+		'good kenward bot' : 'Aww, thanks :)',
+		'good bot' : 'Aww, thanks :)', 
+		'bad kenward bot' : 'hmph..', 
+		'bad bot' : 'hmph..', 
+		'hello' : 'Hey there!'
+	}
+}
+
+module.exports = personality;
